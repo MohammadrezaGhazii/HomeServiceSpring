94959d656e1cad85bd68b1377f1377f3e466b38b
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-07 20:00:37 +0330

    create ServiceManagmentApplication class

---
+++ src/main/java/ir/ghazi/service_managment/ServiceManagmentApplication.java
@@ -0,0 +1,12 @@
+package ir.ghazi.service_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceManagmentApplication.class, args);
+	}
+}
