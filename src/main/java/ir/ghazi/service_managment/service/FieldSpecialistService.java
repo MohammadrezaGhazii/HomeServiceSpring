d33f3127664cb39e0c35e99c4600d7be3ff42e22
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-07 20:34:09 +0330

    create Services for all entity classes

---
+++ src/main/java/ir/ghazi/service_managment/service/FieldSpecialistService.java
@@ -0,0 +1,9 @@
+package ir.ghazi.service_managment.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class FieldSpecialistService {
+}
