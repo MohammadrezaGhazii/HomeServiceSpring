210ca17c0338051ae1b282be3e569860ebfaba3c
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-07 19:58:48 +0330

    add base entity

---
+++ src/main/java/ir/ghazi/service_managment/base/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package ir.ghazi.service_managment.base.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+
+@MappedSuperclass
+public class BaseEntity<ID extends Serializable> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected ID id ;
+}
