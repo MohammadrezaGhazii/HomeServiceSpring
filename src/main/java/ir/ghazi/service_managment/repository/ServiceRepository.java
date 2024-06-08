008a4e5de343719b30fef36dbf2fa89feb852d09
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-08 18:40:33 +0330

    change name service to services

--- src/main/java/ir/ghazi/service_managment/repository/ServiceRepository.java
+++ src/main/java/ir/ghazi/service_managment/repository/ServiceRepository.java
@@ -1,7 +1,7 @@
 package ir.ghazi.service_managment.repository;
 
-import ir.ghazi.service_managment.model.Service;
+import ir.ghazi.service_managment.model.Services;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServiceRepository extends JpaRepository<Service,Long> {
+public interface ServiceRepository extends JpaRepository<Services,Long> {
 }
