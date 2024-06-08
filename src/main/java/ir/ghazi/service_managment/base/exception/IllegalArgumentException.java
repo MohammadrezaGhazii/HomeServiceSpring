0b2690f60a18437faf8f6801871291852ffd5090
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-08 19:48:56 +0330

    add IllegalArgumentException class

---
+++ src/main/java/ir/ghazi/service_managment/base/exception/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package ir.ghazi.service_managment.base.exception;
+
+public class IllegalArgumentException extends RuntimeException{
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
