7c1cdb9391dca8ab1d06fb77c7b0a0d69d008f04
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-09 22:39:34 +0330

    fix test methods for client register

--- src/main/java/ir/ghazi/service_managment/service/ClientService.java
+++ src/main/java/ir/ghazi/service_managment/service/ClientService.java
@@ -1,5 +1,6 @@
 package ir.ghazi.service_managment.service;
 
+import ir.ghazi.service_managment.base.exception.IllegalArgumentException;
 import ir.ghazi.service_managment.base.exception.NotFoundException;
 import ir.ghazi.service_managment.model.Client;
 import ir.ghazi.service_managment.repository.ClientRepository;
@@ -19,19 +20,26 @@ public class ClientService {
     }
 
     public Client saveClient(Client client) {
-        if (!Validation.isNameValid(client.getFirstName()) &&
-                Validation.isNameValid(client.getLastName())) {
-            System.out.println("Firstname and last name should be words only !!!");
-        } else if (!Validation.isEmailValid(client.getEmail())) {
-            System.out.println("Email is not valid ! || Enter like this : johnsons@gmail.com");
-        } else if (!Validation.isPasswordValid(client.getPassword())) {
-            System.out.println("password is not strong ! || Enter like : Aa@12345");
-        } else if (clientRepository.findByEmail(client.getEmail()).isPresent()) {
-            System.out.println("This " + client.getEmail() + " is already registered !!!");
-            System.out.println("Try another email");
-        } else
-            clientRepository.save(client);
-        return client;
+        try {
+            if (!Validation.isNameValid(client.getFirstName())) {
+                log.warn("Firstname should just in letters");
+            } else if (!Validation.isNameValid(client.getLastName())) {
+                log.warn("Lastname should just in letters");
+            } else if (!Validation.isEmailValid(client.getEmail())) {
+                log.warn("Email is not valid ! || Enter like this : johnsons@gmail.com");
+            } else if (!Validation.isPasswordValid(client.getPassword())) {
+                log.warn("password is not strong ! || Enter like : Aa@12345");
+            } else if (clientRepository.findByEmail(client.getEmail()).isPresent()) {
+                log.warn("This " + client.getEmail() + " is already registered !!!");
+            } else {
+                clientRepository.save(client);
+                log.info(client.getEmail() + " has successfully add !");
+            }
+            return client;
+        } catch (Exception e) {
+            log.error("An error in Client sign up ");
+            return null;
+        }
     }
 
     public Optional<Client> clientSignIn(String email, String password) {
@@ -41,7 +49,7 @@ public class ClientService {
             log.info("Welcome " + find.get().getFirstName());
             return find;
         } catch (Exception e) {
-            log.error("An error in Admin sign in ");
+            log.error("An error in Client sign in ");
             return Optional.empty();
         }
     }
--- src/test/java/ir/ghazi/service_managment/service/ClientServiceTest.java
+++ src/test/java/ir/ghazi/service_managment/service/ClientServiceTest.java
@@ -2,19 +2,16 @@ package ir.ghazi.service_managment.service;
 
 import ir.ghazi.service_managment.model.Client;
 import ir.ghazi.service_managment.repository.ClientRepository;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-@Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ClientServiceTest {
 
     @Autowired
@@ -29,18 +26,19 @@ class ClientServiceTest {
         Client client = Client.builder()
                 .firstName("mohammadreza")
                 .lastName("ghazi")
-                .email("ali@gmail.com")
+                .email("testSava2@gmail.com")
                 .password("Aa@12345")
                 .build();
         Client clientSaved = clientService.saveClient(client);
 
-        Optional<Client> byEmailByEmail = clientRepository.findByEmail(clientSaved.getEmail());
-        assertNotNull(byEmailByEmail);
-        assertEquals("mohammadreza", byEmailByEmail.get().getFirstName());
-        assertEquals("ghazi", byEmailByEmail.get().getLastName());
-        assertEquals("ali@gmail.com", byEmailByEmail.get().getEmail());
-        assertEquals("Aa@12345", byEmailByEmail.get().getPassword());
+        Optional<Client> byEmail = clientRepository.findByEmail(clientSaved.getEmail());
+        assertNotNull(byEmail);
+        assertEquals("mohammadreza", byEmail.get().getFirstName());
+        assertEquals("ghazi", byEmail.get().getLastName());
+        assertEquals("testSava2@gmail.com", byEmail.get().getEmail());
+        assertEquals("Aa@12345", byEmail.get().getPassword());
     }
+
     @Test
     @DisplayName("Check Validation name")
     void saveClientValidationName() {
@@ -51,17 +49,26 @@ class ClientServiceTest {
                 .password("Aa@12345")
                 .build();
         clientService.saveClient(client);
+
+        Optional<Client> byEmail = clientRepository.findByEmail(client.getEmail());
+
+        assertFalse(byEmail.isPresent());
     }
+
     @Test
     @DisplayName("Check Validation email")
     void saveClientValidationEmail() {
         Client client = Client.builder()
                 .firstName("saba")
                 .lastName("salmanzade")
-                .email("abcd123")
+                .email("Testemail")
                 .password("Aa@12345")
                 .build();
         clientService.saveClient(client);
+
+        Optional<Client> byEmail = clientRepository.findByEmail(client.getEmail());
+
+        assertFalse(byEmail.isPresent());
     }
 
     @Test
@@ -74,18 +81,22 @@ class ClientServiceTest {
                 .password("zxy5")
                 .build();
         clientService.saveClient(client);
+
+        Optional<Client> byEmail = clientRepository.findByEmail(client.getEmail());
+
+        assertFalse(byEmail.isPresent());
     }
 
     @Test
     @DisplayName("Check for client sign in")
-    void clientSignIn(){
+    void clientSignIn() {
         Optional<Client> client = clientService.clientSignIn("mohammadreza@gmail.com", "Aa@12345");
         assertTrue(client.isPresent());
     }
 
     @Test
     @DisplayName("Check for client not sign in")
-    void clientSignInError(){
+    void clientSignInError() {
         Optional<Client> client = clientService.clientSignIn("notsign@gmail.com", "Aa@12345");
         assertFalse(client.isPresent());
     }
