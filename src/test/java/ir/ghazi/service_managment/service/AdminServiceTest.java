6ac4cba5045c6b5f7fbc3a9ccd464245a3738d20
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-09 22:48:57 +0330

    fix test methods for admin register and log in

--- src/test/java/ir/ghazi/service_managment/service/AdminServiceTest.java
+++ src/test/java/ir/ghazi/service_managment/service/AdminServiceTest.java
@@ -2,9 +2,7 @@ package ir.ghazi.service_managment.service;
 
 import ir.ghazi.service_managment.model.Admin;
 import ir.ghazi.service_managment.repository.AdminRepository;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,7 +11,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AdminServiceTest {
     @Autowired
     private AdminRepository adminRepository;
@@ -21,7 +19,9 @@ class AdminServiceTest {
     @Autowired
     private AdminService adminService;
 
-    @BeforeEach
+    @DisplayName("Check Save Ok")
+    @Order(1)
+    @Test
     public void setAdmin() {
         Admin admin = Admin.builder()
                 .firstName("mohammad")
@@ -30,18 +30,22 @@ class AdminServiceTest {
                 .password("Aa@12345")
                 .build();
         adminRepository.save(admin);
+
+        Optional<Admin> byEmail = adminRepository.findByEmail(admin.getEmail());
+        assertTrue(byEmail.isPresent());
     }
 
     @Test
+    @DisplayName("Check for admin sign in")
     void signInAdmin() {
         Optional<Admin> adminFind = adminService.signInAdmin("mohammad@gmail.com", "Aa@12345");
         assertTrue(adminFind.isPresent());
     }
 
     @Test
+    @DisplayName("Check for client not sign in")
     void signInAdminFalse() {
         Optional<Admin> adminFind = adminService.signInAdmin("ali@gmail.com", "Aa@12345");
-
         assertFalse(adminFind.isPresent());
     }
 }
