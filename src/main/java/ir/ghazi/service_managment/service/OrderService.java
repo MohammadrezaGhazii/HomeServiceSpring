9041e5f167db515cc31cd165950627ffc4245d46
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-12 20:31:31 +0330

    add method for add order with test for it

--- src/main/java/ir/ghazi/service_managment/service/OrderService.java
+++ src/main/java/ir/ghazi/service_managment/service/OrderService.java
@@ -1,9 +1,21 @@
 package ir.ghazi.service_managment.service;
 
+import ir.ghazi.service_managment.model.Order;
+import ir.ghazi.service_managment.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void addOrder(Order order){
+        orderRepository.save(order);
+    }
 }
---
+++ src/test/java/ir/ghazi/service_managment/service/OrderServiceTest.java
@@ -0,0 +1,57 @@
+package ir.ghazi.service_managment.service;
+
+import ir.ghazi.service_managment.enums.OrderSituation;
+import ir.ghazi.service_managment.model.Client;
+import ir.ghazi.service_managment.model.Order;
+import ir.ghazi.service_managment.model.SubService;
+import ir.ghazi.service_managment.repository.ClientRepository;
+import ir.ghazi.service_managment.repository.OrderRepository;
+import ir.ghazi.service_managment.repository.SubServiceRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class OrderServiceTest {
+
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private SubServiceRepository subServiceRepository;
+
+    @Test
+    void addOrder() {
+        Optional<Client> byEmailClient = clientRepository.findByEmail("mohammadreza@gmail.com");
+        Client client = byEmailClient.get();
+        Optional<SubService> byName = subServiceRepository.findByName("Piping");
+        SubService subService = byName.get();
+
+        Order order = Order.builder()
+                .address("Namjoo")
+                .explain("Nothing")
+                .orderDate(LocalDate.now())
+                .orderSituation(OrderSituation.WAIT_FOR_SPECIALIST_OFFER)
+                .requestedDate(LocalDate.of(2024,8,22))
+                .requestedTime(LocalTime.of(20,50))
+                .client(client)
+                .subService(subService)
+                .build();
+        orderService.addOrder(order);
+
+        Optional<Order> byId = orderRepository.findById(1L);
+        assertTrue(byId.isPresent());
+    }
+}
