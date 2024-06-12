0e58a8c0f8db64451d056754da5d6289cf3f021e
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-12 19:56:46 +0330

    add method add field to specialist and test for it

--- src/main/java/ir/ghazi/service_managment/service/FieldSpecialistService.java
+++ src/main/java/ir/ghazi/service_managment/service/FieldSpecialistService.java
@@ -1,9 +1,22 @@
 package ir.ghazi.service_managment.service;
 
+import ir.ghazi.service_managment.model.FieldSpecialist;
+import ir.ghazi.service_managment.repository.FieldSpecialistRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 public class FieldSpecialistService {
+    private final FieldSpecialistRepository fieldSpecialistRepository;
+
+    public FieldSpecialistService(FieldSpecialistRepository fieldSpecialistRepository) {
+        this.fieldSpecialistRepository = fieldSpecialistRepository;
+    }
+
+    public void addFieldSpecialist(FieldSpecialist fieldSpecialist){
+        fieldSpecialistRepository.save(fieldSpecialist);
+    }
+
 }
---
+++ src/test/java/ir/ghazi/service_managment/service/FieldSpecialistServiceTest.java
@@ -0,0 +1,66 @@
+package ir.ghazi.service_managment.service;
+
+import ir.ghazi.service_managment.model.FieldSpecialist;
+import ir.ghazi.service_managment.model.Specialist;
+import ir.ghazi.service_managment.model.SubService;
+import ir.ghazi.service_managment.repository.FieldSpecialistRepository;
+import ir.ghazi.service_managment.repository.SpecialistRepository;
+import ir.ghazi.service_managment.repository.SubServiceRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class FieldSpecialistServiceTest {
+
+    @Autowired
+    private FieldSpecialistService fieldSpecialistService;
+    @Autowired
+    private FieldSpecialistRepository fieldSpecialistRepository;
+
+    @Autowired
+    private SpecialistRepository specialistRepository;
+    @Autowired
+    private SubServiceRepository subServiceRepository;
+
+    @Test
+    @Order(1)
+    @DisplayName("add field to specialist")
+    void addFieldSpecialist() {
+        Optional<Specialist> byEmailSpecialist = specialistRepository.findByEmail("mohammadreza@gmail.com");
+        Specialist specialist = byEmailSpecialist.get();
+        Optional<SubService> byNameSubService = subServiceRepository.findByName("Piping");
+        SubService subService = byNameSubService.get();
+        Optional<SubService> byNameSubService1 = subServiceRepository.findByName("phone repairs");
+        SubService subService1 = byNameSubService1.get();
+
+        FieldSpecialist fieldSpecialist = FieldSpecialist.builder()
+                .specialist(specialist)
+                .subService(subService)
+                .build();
+
+        fieldSpecialistService.addFieldSpecialist(fieldSpecialist);
+        Optional<FieldSpecialist> byId = fieldSpecialistRepository.findById(1L);
+
+        FieldSpecialist fieldSpecialist1 = FieldSpecialist.builder()
+                .specialist(specialist)
+                .subService(subService1)
+                .build();
+
+        fieldSpecialistService.addFieldSpecialist(fieldSpecialist1);
+        Optional<FieldSpecialist> byId1 = fieldSpecialistRepository.findById(2L);
+
+        assertTrue(byId.isPresent());
+        assertTrue(byId1.isPresent());
+    }
+
+    @Test
+    @DisplayName("delete field to specialist")
+    void deleteFieldSpecialist(){
+    }
+}
