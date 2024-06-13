ec146e03b9ac5dad81446e7bf0f4bf10b9b81b31
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-13 18:15:28 +0330

    remove useless try catch

--- src/main/java/ir/ghazi/service_managment/service/SpecialistService.java
+++ src/main/java/ir/ghazi/service_managment/service/SpecialistService.java
@@ -21,28 +21,23 @@ public class SpecialistService {
     }
 
     public Specialist saveSpecialist(Specialist specialist) {
-        try {
-            if (!Validation.isNameValid(specialist.getFirstName())) {
-                log.warn("Firstname should just in letters");
-            } else if (!Validation.isNameValid(specialist.getLastName())) {
-                log.warn("Lastname should just in letters");
-            } else if (!Validation.isEmailValid(specialist.getEmail())) {
-                log.warn("Email is not valid ! || Enter like this : johnsons@gmail.com");
-            } else if (!Validation.isPasswordValid(specialist.getPassword())) {
-                log.warn("password is not strong ! || Enter like : Aa@12345");
-            } else if (specialistRepository.findByEmail(specialist.getEmail()).isPresent()) {
-                log.warn("This " + specialist.getEmail() + " is already registered !!!");
-            } else if (specialist.getImage() == null) {
-                log.warn("This " + specialist.getEmail() + " photo is not available !!!");
-            } else {
-                specialistRepository.save(specialist);
-                log.info(specialist.getEmail() + " has successfully add !");
-            }
-            return specialist;
-        } catch (Exception e) {
-            log.error("An error in Specialist sign up ");
-            return null;
+        if (!Validation.isNameValid(specialist.getFirstName())) {
+            log.warn("Firstname should just in letters");
+        } else if (!Validation.isNameValid(specialist.getLastName())) {
+            log.warn("Lastname should just in letters");
+        } else if (!Validation.isEmailValid(specialist.getEmail())) {
+            log.warn("Email is not valid ! || Enter like this : johnsons@gmail.com");
+        } else if (!Validation.isPasswordValid(specialist.getPassword())) {
+            log.warn("password is not strong ! || Enter like : Aa@12345");
+        } else if (specialistRepository.findByEmail(specialist.getEmail()).isPresent()) {
+            log.warn("This " + specialist.getEmail() + " is already registered !!!");
+        } else if (specialist.getImage() == null) {
+            log.warn("This " + specialist.getEmail() + " photo is not available !!!");
+        } else {
+            specialistRepository.save(specialist);
+            log.info(specialist.getEmail() + " has successfully add !");
         }
+        return specialist;
     }
 
     private String getContentType(File file) {
@@ -60,34 +55,29 @@ public class SpecialistService {
     public byte[] uploadPhoto(String filePath) {
         byte[] photo = null;
         while (true) {
-            try {
-                File file = new File(filePath);
-                if (file.exists() && file.isFile()) {
-                    if (file.length() <= 300 * 1024) {
-                        String fileType = getContentType(file);
-                        if (fileType.equals("image/jpeg")) {
-                            try {
-                                photo = Files.readAllBytes(file.toPath());
-                                break;
-                            } catch (Exception e) {
-                                log.error("Error reading file: " + e.getMessage());
-                                break;
-                            }
-                        } else {
-                            log.error("Invalid file format. Please upload a JPG file.");
+            File file = new File(filePath);
+            if (file.exists() && file.isFile()) {
+                if (file.length() <= 300 * 1024) {
+                    String fileType = getContentType(file);
+                    if (fileType.equals("image/jpeg")) {
+                        try {
+                            photo = Files.readAllBytes(file.toPath());
+                            break;
+                        } catch (Exception e) {
+                            log.error("Error reading file: " + e.getMessage());
                             break;
                         }
                     } else {
-                        log.error("File size exceeds 300KB. Please upload a smaller file.");
+                        log.error("Invalid file format. Please upload a JPG file.");
                         break;
                     }
                 } else {
-                    log.error("Invalid file path. Please try again.");
+                    log.error("File size exceeds 300KB. Please upload a smaller file.");
                     break;
                 }
-            } catch (Exception e) {
-                log.error("An error in Specialist sign up ");
-                return null;
+            } else {
+                log.error("Invalid file path. Please try again.");
+                break;
             }
         }
         return photo;
