bf744059c5efbd1b6861dad3391f5cb3f4e025a4
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-15 22:51:53 +0330

    add list for order

--- src/main/java/ir/ghazi/service_managment/repository/OrderRepository.java
+++ src/main/java/ir/ghazi/service_managment/repository/OrderRepository.java
@@ -1,8 +1,12 @@
 package ir.ghazi.service_managment.repository;
 
 import ir.ghazi.service_managment.model.Order;
+import ir.ghazi.service_managment.model.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findBySubService (SubService subService);
 }
--- src/main/java/ir/ghazi/service_managment/service/OrderService.java
+++ src/main/java/ir/ghazi/service_managment/service/OrderService.java
@@ -1,21 +1,47 @@
 package ir.ghazi.service_managment.service;
 
+import ir.ghazi.service_managment.model.FieldSpecialist;
 import ir.ghazi.service_managment.model.Order;
+import ir.ghazi.service_managment.model.Specialist;
+import ir.ghazi.service_managment.model.SubService;
+import ir.ghazi.service_managment.repository.FieldSpecialistRepository;
 import ir.ghazi.service_managment.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final FieldSpecialistRepository fieldSpecialistRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, FieldSpecialistRepository fieldSpecialistRepository) {
         this.orderRepository = orderRepository;
+        this.fieldSpecialistRepository = fieldSpecialistRepository;
     }
 
-    public void addOrder(Order order){
+    public void addOrder(Order order) {
         orderRepository.save(order);
+    }
+
+    public void listOrderBySubServiceAndSpecialist(SubService subService, Specialist specialist) {
+        List<Order> bySubServiceOrder = orderRepository.findBySubService(subService);
+
+        for (Order order : bySubServiceOrder) {
+            SubService subService1 = order.getSubService();
+            Optional<FieldSpecialist> bySpecialistAndSubService =
+                    fieldSpecialistRepository.findBySpecialistAndSubService(specialist, subService1);
+
+            if (bySpecialistAndSubService.isPresent()) {
+                System.out.println("Order Sub Service : " + order.getSubService().getName() +
+                        " Explain " + order.getExplain() +
+                        " Order Address : " + order.getAddress() +
+                        " Requested Date : " + order.getRequestedDate() +
+                        " Requested Time : " + order.getRequestedTime());
+            }
+        }
     }
 }
