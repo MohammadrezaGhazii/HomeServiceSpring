4f4831a3deeefa8e191a4f14534eac389365177d
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-16 17:09:49 +0330

    add methods for list offers with score specialist or offer price

--- src/main/java/ir/ghazi/service_managment/service/OfferService.java
+++ src/main/java/ir/ghazi/service_managment/service/OfferService.java
@@ -4,9 +4,13 @@ import ir.ghazi.service_managment.enums.OrderSituation;
 import ir.ghazi.service_managment.model.*;
 import ir.ghazi.service_managment.repository.FieldSpecialistRepository;
 import ir.ghazi.service_managment.repository.OfferRepository;
+import ir.ghazi.service_managment.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -16,11 +20,13 @@ public class OfferService {
     private final OfferRepository offerRepository;
     private final FieldSpecialistRepository fieldSpecialistRepository;
     private final OrderService orderService;
+    private final OrderRepository orderRepository;
 
-    public OfferService(OfferRepository offerRepository, FieldSpecialistRepository fieldSpecialistRepository, OrderService orderService) {
+    public OfferService(OfferRepository offerRepository, FieldSpecialistRepository fieldSpecialistRepository, OrderService orderService, OrderRepository orderRepository) {
         this.offerRepository = offerRepository;
         this.fieldSpecialistRepository = fieldSpecialistRepository;
         this.orderService = orderService;
+        this.orderRepository = orderRepository;
     }
 
     public void saveOffer(Offer offer, Order order, Specialist specialist) {
@@ -41,5 +47,39 @@ public class OfferService {
         }
         else
             offerRepository.save(offer);
+    }
+
+    public List<Double> listByOfferPrice(Long id){
+        Optional<Order> byId = orderRepository.findById(id);
+        Order order = byId.get();
+
+        List<Offer> offersByOrder = offerRepository.findByOrder(order);
+        List<Double> offerPrices = new ArrayList<>();
+        for (Offer offer : offersByOrder) {
+            Double offerPrice = offer.getOfferPrice();
+            offerPrices.add(offerPrice);
+        }
+        Collections.sort(offerPrices);
+        for (Double offerPrice : offerPrices) {
+            log.info(String.valueOf(offerPrice));
+        }
+        return offerPrices;
+    }
+
+    public List<Double> listByScoreSpecialist(Long id){
+        Optional<Order> byId = orderRepository.findById(id);
+        Order order = byId.get();
+
+        List<Offer> offersByOrder = offerRepository.findByOrder(order);
+        List<Double> offerSpecialistScores = new ArrayList<>();
+        for (Offer offer : offersByOrder) {
+            Double score = offer.getSpecialist().getScore();
+            offerSpecialistScores.add(score);
+        }
+        Collections.sort(offerSpecialistScores);
+        for (Double offerSpecialistScore : offerSpecialistScores) {
+            log.info(String.valueOf(offerSpecialistScore));
+        }
+        return offerSpecialistScores;
     }
 }
