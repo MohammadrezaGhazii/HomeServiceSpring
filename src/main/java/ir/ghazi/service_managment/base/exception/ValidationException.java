913eac57561e214ad97be72770e39891582e0fc9
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-21 17:52:54 +0330

    create ValidationException class

---
+++ src/main/java/ir/ghazi/service_managment/base/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package ir.ghazi.service_managment.base.exception;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
