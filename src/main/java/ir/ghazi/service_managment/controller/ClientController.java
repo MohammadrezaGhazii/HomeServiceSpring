f829596a4f172d057c81026e527218cf765cdea6
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-21 18:36:46 +0330

    add registerClient method

--- src/main/java/ir/ghazi/service_managment/controller/ClientController.java
+++ src/main/java/ir/ghazi/service_managment/controller/ClientController.java
@@ -1,7 +1,15 @@
 package ir.ghazi.service_managment.controller;
 
+import ir.ghazi.service_managment.dto.client.ClientRequest;
+import ir.ghazi.service_managment.dto.client.ClientResponse;
+import ir.ghazi.service_managment.mapper.ClientMapper;
+import ir.ghazi.service_managment.model.Client;
 import ir.ghazi.service_managment.service.ClientService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +18,10 @@ public class ClientController {
 
     private final ClientService clientService;
 
-
+    @PostMapping("/register-client")
+    public ResponseEntity<ClientResponse> registerClient(@RequestBody ClientRequest request) {
+        Client mappedClient = ClientMapper.INSTANCE.clientSaveRequestToModel(request);
+        Client savedClient = clientService.saveClient(mappedClient);
+        return new ResponseEntity<>(ClientMapper.INSTANCE.modelToClientSaveResponse(savedClient), HttpStatus.CREATED);
+    }
 }
--- src/main/java/ir/ghazi/service_managment/mapper/ClientMapper.java
+++ src/main/java/ir/ghazi/service_managment/mapper/ClientMapper.java
@@ -3,8 +3,9 @@ package ir.ghazi.service_managment.mapper;
 import ir.ghazi.service_managment.dto.client.ClientRequest;
 import ir.ghazi.service_managment.dto.client.ClientResponse;
 import ir.ghazi.service_managment.model.Client;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-
+@Mapper
 public interface ClientMapper {
     ClientMapper INSTANCE = Mappers.getMapper(ClientMapper.class);
 
