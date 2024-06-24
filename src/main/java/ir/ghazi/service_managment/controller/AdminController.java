b47bf99bd3569e8ee49d8a2e0d14dfe943d4e6bf
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-24 16:38:51 +0330

    add mapper for method save service and subservice

--- src/main/java/ir/ghazi/service_managment/controller/AdminController.java
+++ src/main/java/ir/ghazi/service_managment/controller/AdminController.java
@@ -2,7 +2,13 @@ package ir.ghazi.service_managment.controller;
 
 import ir.ghazi.service_managment.dto.admin.AdminRequest;
 import ir.ghazi.service_managment.dto.admin.AdminResponse;
+import ir.ghazi.service_managment.dto.service.ServiceRequest;
+import ir.ghazi.service_managment.dto.service.ServiceResponse;
+import ir.ghazi.service_managment.dto.subservice.SubServiceRequest;
+import ir.ghazi.service_managment.dto.subservice.SubServiceResponse;
 import ir.ghazi.service_managment.mapper.AdminMapper;
+import ir.ghazi.service_managment.mapper.ServiceMapper;
+import ir.ghazi.service_managment.mapper.SubServiceMapper;
 import ir.ghazi.service_managment.model.Admin;
 import ir.ghazi.service_managment.model.FieldSpecialist;
 import ir.ghazi.service_managment.model.Services;
@@ -40,35 +46,37 @@ public class AdminController {
     }
 
     @PostMapping("/add-service")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void addService(@RequestBody Services services){
-        serviceService.saveService(services);
+    public ResponseEntity<ServiceResponse> addService(@RequestBody ServiceRequest request) {
+        Services mappedService = ServiceMapper.INSTANCE.serviceSaveRequestToModel(request);
+        Services savedService = serviceService.saveService(mappedService);
+        return new ResponseEntity<>(ServiceMapper.INSTANCE.modelToServiceSaveResponse(savedService), HttpStatus.CREATED);
     }
 
     @PostMapping("/add-sub-service")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void addSubService(@RequestBody SubService subService){
-        subServiceService.savaSubService(subService);
+    public ResponseEntity<SubServiceResponse> addSubService(@RequestBody SubServiceRequest request) {
+        SubService mappedSubService = SubServiceMapper.INSTANCE.subServiceSaveRequestToModel(request);
+        SubService savedSubService = subServiceService.savaSubService(mappedSubService);
+        return new ResponseEntity<>(SubServiceMapper.INSTANCE.modelToSubServiceSaveResponse(savedSubService), HttpStatus.CREATED);
     }
 
     @PostMapping("/add-field-specialist")
     @ResponseStatus(HttpStatus.CREATED)
-    public void addFieldSpecialist(@RequestBody FieldSpecialist fieldSpecialist){
+    public void addFieldSpecialist(@RequestBody FieldSpecialist fieldSpecialist) {
         fieldSpecialistService.addFieldSpecialist(fieldSpecialist);
     }
 
     @GetMapping("/list-services")
-    public void listService(){
+    public void listService() {
         serviceService.listServices();
     }
 
     @GetMapping("/list-sub-services")
-    public void listSubService(){
+    public void listSubService() {
         subServiceService.subServiceList();
     }
 
     @PatchMapping("/approve-specialist")
-    public void approveSpecialist(@RequestParam String email){
+    public void approveSpecialist(@RequestParam String email) {
         specialistService.acceptSpecialist(email);
     }
 }
