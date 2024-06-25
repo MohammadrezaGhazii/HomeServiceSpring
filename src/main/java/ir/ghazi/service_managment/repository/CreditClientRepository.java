f91ed039ee421efcb968bb8d56f7793cf101dbe2
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-25 12:19:03 +0330

    add method for findByClient

--- src/main/java/ir/ghazi/service_managment/repository/CreditClientRepository.java
+++ src/main/java/ir/ghazi/service_managment/repository/CreditClientRepository.java
@@ -1,7 +1,11 @@
 package ir.ghazi.service_managment.repository;
 
+import ir.ghazi.service_managment.model.Client;
 import ir.ghazi.service_managment.model.CreditClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CreditClientRepository extends JpaRepository<CreditClient, Long> {
+    Optional<CreditClient> findByClient (Client client);
 }
--- src/main/java/ir/ghazi/service_managment/service/CreditClientService.java
+++ src/main/java/ir/ghazi/service_managment/service/CreditClientService.java
@@ -1,10 +1,13 @@
 package ir.ghazi.service_managment.service;
 
+import ir.ghazi.service_managment.model.Client;
 import ir.ghazi.service_managment.model.CreditClient;
 import ir.ghazi.service_managment.repository.CreditClientRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -14,5 +17,10 @@ public class CreditClientService {
     private final CreditClientRepository clientRepository;
     public void addCredit(CreditClient creditClient){
         clientRepository.save(creditClient);
+    }
+
+    public CreditClient findByClient(Client client){
+        Optional<CreditClient> byClient = clientRepository.findByClient(client);
+        return byClient.get();
     }
 }
