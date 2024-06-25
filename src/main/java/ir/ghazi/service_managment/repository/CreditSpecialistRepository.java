ed2fd55320d2571d5c75c9714da28d2c0fc31a56
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-25 12:20:41 +0330

    add method for findBySpecialist

--- src/main/java/ir/ghazi/service_managment/repository/CreditSpecialistRepository.java
+++ src/main/java/ir/ghazi/service_managment/repository/CreditSpecialistRepository.java
@@ -1,7 +1,11 @@
 package ir.ghazi.service_managment.repository;
 
 import ir.ghazi.service_managment.model.CreditSpecialist;
+import ir.ghazi.service_managment.model.Specialist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CreditSpecialistRepository extends JpaRepository<CreditSpecialist, Long> {
+    Optional<CreditSpecialist> findBySpecialist (Specialist specialist);
 }
--- src/main/java/ir/ghazi/service_managment/service/CreditSpecialistService.java
+++ src/main/java/ir/ghazi/service_managment/service/CreditSpecialistService.java
@@ -2,11 +2,14 @@ package ir.ghazi.service_managment.service;
 
 import ir.ghazi.service_managment.model.CreditClient;
 import ir.ghazi.service_managment.model.CreditSpecialist;
+import ir.ghazi.service_managment.model.Specialist;
 import ir.ghazi.service_managment.repository.CreditClientRepository;
 import ir.ghazi.service_managment.repository.CreditSpecialistRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -17,5 +20,10 @@ public class CreditSpecialistService {
 
     public void addCredit(CreditSpecialist creditSpecialist){
         creditSpecialistRepository.save(creditSpecialist);
+    }
+
+    public CreditSpecialist findBySpecialist(Specialist specialist){
+        Optional<CreditSpecialist> bySpecialist = creditSpecialistRepository.findBySpecialist(specialist);
+        return bySpecialist.get();
     }
 }
