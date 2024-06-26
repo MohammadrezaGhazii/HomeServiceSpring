67dde3def71cf4a7137d4471e234b63eda490ead
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-26 19:00:57 +0330

    add GlobalExceptionHandler

---
+++ src/main/java/ir/ghazi/service_managment/base/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package ir.ghazi.service_managment.base.exception;
+
+import ir.ghazi.service_managment.dto.exception.ExceptionDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler({ValidationException.class })
+    public ResponseEntity<ExceptionDto> duplicateInformationExceptionHandler(ValidationException e) {
+        log.warn(e.getMessage());
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler({NotFoundException.class })
+    public ResponseEntity<ExceptionDto> duplicateInformationExceptionHandler(NotFoundException e) {
+        log.warn(e.getMessage());
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.CONFLICT);
+    }
+}
