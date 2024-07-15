b668051ee595e04f5479fee6b2478e7ccb049698
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-07-15 20:48:51 +0330

    create class SecurityConfiguration

---
+++ src/main/java/ir/ghazi/service_managment/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package ir.ghazi.service_managment.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests
+                        (s -> s.anyRequest().permitAll()).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
